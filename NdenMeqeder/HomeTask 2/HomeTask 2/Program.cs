0d78ae2cdc4e607625107f16187e175f452253ce
SamirAliyevv <samirsa@code.edu.az>
2023-01-17 15:36:48 -0800

    master

---
+++ NdenMeqeder/HomeTask 2/HomeTask 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace HomeTask_2
+{
+    internal class Program
+    {
+
+        static int NdenMe(int n, int m)
+        {
+
+            int sum = 0;
+            int count = 0;
+
+            if (n < m)
+            {
+
+                for (int i = n; i < m; i++)
+                {
+                    if (i % 21 == 0)
+                    {
+                        sum += i;
+                        count++;
+
+                    }
+
+
+
+                }
+
+            }
+
+
+            return sum / count;
+
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            //N den M e qeder 21 e bolunen ededlerin ededi ortasini tapan proqram
+
+            Console.WriteLine("Zehmet olmasa ededleri daxil edin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(NdenMe(n, m));
+
+
+        }
+    }
+    
+
+   
+    
+
+
+    
+}
---
+++ Verilmis n tam ededini reqemleri cemin tapan proqram/Verilmis n tam ededini reqemleri cemin tapan proqram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Verilmis_n_tam_ededini_reqemleri_cemin_tapan_proqram
+{
+    internal class Program
+    {
+        static int DigitSum()
+        {
+            int number = 2345;
+            int sum = 0;
+            int digit = 0;
+            while (number > 0)
+            {
+                digit = number % 10;
+                number /= 10;
+                sum += digit;
+
+            }
+
+           
+
+
+
+            return sum;
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitSum());
+            
+        } 
+
+
+    }
+}
